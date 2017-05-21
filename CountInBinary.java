457bbf8a5a5b319e01473abc72d6a0c3b95962ef
vinamratadake <vinamratadake@gmail.com>
2017-05-20 22:23:22 -0700

    my first commit

---
+++ CountInBinary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by vinamrata on 12/30/16.
+ */
+public class CountInBinary {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int counter=0, max =0;
+
+        while(num > 0){
+            int rem = num % 2;
+            if(rem == 1) {
+                counter++;
+            }
+            else
+                counter = 0;
+            max = Math.max(counter,max);
+            num = num / 2;
+        }
+        System.out.println(max);
+
+    }
+
+}
